f81180ea2d67a9ef5fc53b98242e862354a00a41
Jessica Byrne <jessica.byrne@walmartlabs.com>
2019-12-06 10:38:14 -0500

    day 2

---
+++ day2.swift/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+var input = [1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,13,1,19,1,6,19,23,2,23,6,27,1,5,27,31,1,10,31,35,2,6,35,39,1,39,13,43,1,43,9,47,2,47,10,51,1,5,51,55,1,55,10,59,2,59,6,63,2,6,63,67,1,5,67,71,2,9,71,75,1,75,6,79,1,6,79,83,2,83,9,87,2,87,13,91,1,10,91,95,1,95,13,99,2,13,99,103,1,103,10,107,2,107,10,111,1,111,9,115,1,115,2,119,1,9,119,0,99,2,0,14,0]
+
+input[1] = 12
+input[2] = 2
+
+func IntcodeProgram(_ input: [Int], _ idx: Int = 0) -> [Int] {
+    let opcode = input[idx]
+    if opcode == 99 { return input }
+    let x = input[input[idx+1]]
+    let y = input[input[idx+2]]
+    let out = input[idx+3]
+    
+    var result : [Int] = input
+    switch opcode {
+    case 1: result[out] = x+y
+    case 2: result[out] = x*y
+    default: return result
+    }
+    
+    return IntcodeProgram(result, idx+4)
+}
+print(IntcodeProgram(input))
